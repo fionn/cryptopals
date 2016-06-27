7d9401721ba180c80ed9fc47bed072572c29bb87
Fionn Fitzmaurice <iamfionn@github.com>
2016-06-27 16:55:02 +0100

    aes in ecb mode -- spacing

--- m07.py
+++ m07.py
@@ -5,12 +5,10 @@ from base64 import b64decode
 from Crypto.Cipher import AES
 
 if __name__ == "__main__":
-    
     cyphertext = b64decode(open("data/07.txt", "r").read())
     key = bytes("YELLOW SUBMARINE", "utf8")
 
     cypher = AES.new(key, AES.MODE_ECB)
-
     cleartext = cypher.decrypt(cyphertext)
 
     print(cleartext.decode())

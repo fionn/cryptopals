52e0a8e497108f67c27581bef5aa4fe3dfd62559
Fionn Fitzmaurice <iamfionn@github.com>
2016-06-27 03:53:49 +0100

    repeating-key xor now all in binary

--- m05.py
+++ m05.py
@@ -8,11 +8,7 @@ from m02 import fixed_xor
 
 def repeating_key_xor(key, message):
     key = (key * (int(len(message) / len(key)) + 1))[:len(message)]
-
-    key_bytes, message_bytes = bytes(key, "utf8"), bytes(message, "utf8")
-
-    return fixed_xor(key_bytes, message_bytes)
-
+    return fixed_xor(key, message)
 
 if __name__ == "__main__":
 
@@ -22,6 +18,8 @@ if __name__ == "__main__":
         f = open("data/05.txt", "r").read().rstrip()
 
     k = "ICE"
+
+    k, f = bytes(k, "utf8"), bytes(f, "utf8")
     c = repeating_key_xor(k, f)
 
     print(textwrap.fill(hexlify(c).decode(), 75))

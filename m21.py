44abe92c61319565201a45da8681968444ee4c84
Fionn Fitzmaurice <iamfionn@github.com>
2017-05-15 04:59:09 +0100

    mt: temper function

--- m21.py
+++ m21.py
@@ -2,32 +2,42 @@
 # The Mersenne Twister
 
 class MT19937:
+    u = 11
+    s, b = 7,  0x9d2c5680
+    t, c = 15, 0xefc60000
+    l = 18
 
     def __init__(self, seed):
         self.index = 624
-        self.mt = [0] * 624
-        self.mt[0] = seed & 0xffffffff
+        self.state = [0] * 624
+        self.state[0] = seed & 0xffffffff
         for i in range(1, 624):
-            self.mt[i] = 1812433253 * (self.mt[i - 1] ^ self.mt[i - 1] >> 30) + i \
-                         & 0xffffffff
+            self.state[i] = 0x6c078965 \
+                            * (self.state[i - 1] ^ self.state[i - 1] >> 30) \
+                            + i & 0xffffffff
+
+    @staticmethod
+    def temper(y):
+        y ^= y >> MT19937.u
+        y ^= y << MT19937.s & MT19937.b  
+        y ^= y << MT19937.t & MT19937.c 
+        y ^= y >> MT19937.l
+        return y
 
     def random(self):
         if self.index >= 624:
-            self.twist()
-        y = self.mt[self.index]
-        y ^= y >> 11
-        y ^= y << 7 & 0x9d2c5680  #2636928640
-        y ^= y << 15 & 0xefc60000 #4022730752
-        y ^= y >> 18
+            self.__twist()
+        y = self.temper(self.state[self.index])
         self.index += 1
         return y
 
-    def twist(self):
+    def __twist(self):
         for i in range(624):
-            y = (self.mt[i] & 0x80000000) + (self.mt[(i + 1) % 624] & 0x7fffffff)
-            self.mt[i] = self.mt[(i + 397) % 624] ^ y >> 1
+            y = (self.state[i] & 0x80000000) \
+                + (self.state[(i + 1) % 624] & 0x7fffffff)
+            self.state[i] = self.state[(i + 397) % 624] ^ y >> 1
             if y % 2 != 0:
-                self.mt[i] ^= 0x9908b0df
+                self.state[i] ^= 0x9908b0df
         self.index = 0
 
 if __name__ == "__main__":

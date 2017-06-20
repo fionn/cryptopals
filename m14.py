13412c02d277315a818fb1f5bd7779570b7ad411
Fionn Fitzmaurice <iamfionn@github.com>
2017-06-20 23:00:32 +0800

    deduplication of m12.len_string

--- m14.py
+++ m14.py
@@ -6,7 +6,7 @@ from Crypto.Random.random import getrandbits, randrange
 from Crypto.Cipher import AES
 from m09 import pkcs7, de_pkcs7
 from m11 import detect_ecb
-from m12 import blocksize
+from m12 import blocksize, len_string
 
 RANDOM_KEY = bytes(getrandbits(8) for i in range(16))
 RANDOM_PREFIX = bytes(getrandbits(8) for i in range(randrange(16)))
@@ -21,29 +21,22 @@ def decrypt(cyphertext):
     cypher = AES.new(RANDOM_KEY, AES.MODE_ECB)
     return de_pkcs7(cypher.decrypt(cyphertext))
 
-def len_prefix(oracle): 
+def len_prefix(oracle):
     for i in range(32, 48):
         c = oracle(i * b'A')
         for b in range(len(c) // 16 - 1):
-            if c[(b+1)*16:(b+2)*16] == c[(b+2)*16:(b+3)*16]:
+            if c[(b + 1) * 16 : (b + 2) * 16] == c[(b + 2) * 16 : (b + 3) * 16]:
                 return 48 - i + 16 * b
     return 0
-
-def len_string(oracle):
-    l = len(oracle())
-    bs = blocksize(oracle)
-    for i in range(1, bs + 1): 
-        if l < len(oracle(i * b'A')):
-            return l - i - len_prefix(oracle)
 
 def break_ecb(oracle):
     bs = blocksize(oracle)
     l = len(oracle())
-    string_length = len_string(oracle)
     prefix_length = len_prefix(oracle)
+    string_length = len_string(oracle) - prefix_length
 
     plaintext = b''
-    uc = (l + bs - len_prefix(oracle) - 1) * b'A'
+    uc = (l + bs - prefix_length - 1) * b'A'
     while len(plaintext) <= string_length:
         oracle_input = oracle(uc)
         for i in range(127):
